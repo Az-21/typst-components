676f9bee2687b7ac82be724e3e67f9d7e9ae8fb5
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-09-05 16:43:57 +0530

    Add sync direction option
    Editing @local enables live changes

--- sync.py
+++ sync.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import sys
+from enum import Enum
 
 
 def ensure_windows():
@@ -20,21 +21,48 @@ def delete_folder_if_exists(folder_path):
 def copy_folder(src, dst):
   try:
     shutil.copytree(src, dst)
-    print(f"Synced {src} with {dst}")
+    print(f"Synced {src} -> {dst}")
   except Exception as e:
     print(f"Error syncing: {e}")
+    sys.exit()
 
 
-def main():
+class SyncMode(Enum):
+  FromGitToLocal = 0
+  FromLocalToGit = 1
+
+
+def parse_cli_args(args: list[str]) -> SyncMode:
+  arg_count: int = len(args) - 1  # 0th arg is the .py file
+  if arg_count != 1:
+    print(f"[ FATAL ] Expected 1 sync mode argument, got {arg_count}")
+    sys.exit()
+
+  mode: str = args[1]
+  if mode == "--from-git-to-local":
+    return SyncMode.FromGitToLocal
+  if mode == "--from-local-to-git":
+    return SyncMode.FromLocalToGit
+
+  print("[ FATAL ] Unexpected mode passed")
+  sys.exit()
+
+
+def main(args: list[str]) -> None:
   ensure_windows()
 
   local_appdata = os.getenv("LOCALAPPDATA")
-  target_folder = os.path.join(local_appdata, "typst", "packages", "local", "style")
-  source_folder = "./style/"
+  local_style_folder: str = os.path.join(local_appdata, "typst", "packages", "local", "style")
+  git_style_folder = "./style/"
 
-  delete_folder_if_exists(target_folder)
-  copy_folder(source_folder, target_folder)
+  sync_mode: SyncMode = parse_cli_args(args)
+  if sync_mode is SyncMode.FromGitToLocal:
+    delete_folder_if_exists(local_style_folder)
+    copy_folder(git_style_folder, local_style_folder)
+  if sync_mode is SyncMode.FromLocalToGit:
+    delete_folder_if_exists(git_style_folder)
+    copy_folder(local_style_folder, git_style_folder)
 
 
 if __name__ == "__main__":
-  main()
+  main(sys.argv)
